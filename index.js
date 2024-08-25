40cd18db1890e50837e8e701330b42bd5cf9319f
ahmed <01204054667apc@gmail.com>
2024-08-25 20:44:32 +0300

    del

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 dotenv.config({path:path.join(__dirname, "/.env")});
 connectDB();
 
-const port=process.env.PORT||5000;
+const PORT=process.env.PORT||5000;
 
 cloudinary.config({
     cloud_name:process.env.CloudINARY_CLOUD_NAME,
@@ -26,14 +26,12 @@ app.use(cors())
 app.use(express.json({limit:"80mb"}));
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
-if (process.env.MODE=='development') {
-    app.use(morgan('dev'))
-    
-}
+
 app.use('/api/users',userRoutes)
 app.use('/api/posts',postRoutes)
 app.use('/api/messages',messageRoutes)
+app.all('*', (req, res, next) => {
+    res.send("In-valid Routing Plz check url  or  method")
+})
 
-	
-
-server.listen(port,()=>console.log(`server startde at http://localhost:${port}`));
+server.listen(PORT,()=>console.log(`server startde at http://localhost:${PORT}`));
