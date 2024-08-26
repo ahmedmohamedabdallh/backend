d9900d579b0ff915a4e3be9c552d701635c7acc8
ahmed <01204054667apc@gmail.com>
2024-08-26 17:54:38 +0300

    app

--- index.router.js
+++ index.router.js
@@ -2,10 +2,18 @@ import connectDB from './db/connectDB.js'
 import userRoutes from "./routes/userRoutes.js"
 import postRoutes from "./routes/postRoutes.js"
 import messageRoutes from "./routes/messageRoutes.js"
-
+import { v2 as cloudinary } from 'cloudinary'
+import cookieParser from "cookie-parser";
 const initApp = (app, express) => {
-    //convert Buffer Data
-    app.use(express.json({}))
+    cloudinary.config({
+        cloud_name:process.env.CloudINARY_CLOUD_NAME,
+        api_key:process.env.CloudINARY_API_KEY,
+        api_secret:process.env.CloudINARY_API_SECRET
+    
+    })
+    app.use(express.json({limit:"80mb"}))
+    app.use(express.urlencoded({extended:true}));
+    app.use(cookieParser());
     //Setup API Routing 
     app.use(`/users`,userRoutes)
     app.use(`/posts`,postRoutes)
